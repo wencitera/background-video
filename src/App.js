5148479b8a39a47e275ce432c0faf63bc734f623
Wenceslao Ariel Citera <wencitera@gmail.com>
2021-06-06 19:58:39 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import video from './video.mp4'
+
+function pauseResumeVideo() {
+  var element = document.getElementById('video');
+  var btn = document.getElementById('btn');
+  if (element.paused) {
+    element.play();
+    btn.style.backgroundColor = "#c20000";
+    btn.textContent = "Pausar video";
+  } else {
+    element.pause();
+    btn.style.backgroundColor = "#0d8500";
+    btn.textContent = "Resumir video";
+  }
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="MainBanner" className="fondo">
+      <video id='video' autoPlay muted loop>
+        <source src={video} type="video/mp4" />
+      </video>
+      <div className="content">
+        <button className="button" id="btn" onClick={()=>{pauseResumeVideo()}}>Pausar Video</button>
+      </div>
     </div>
   );
 }
